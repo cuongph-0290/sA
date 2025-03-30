27b7400cabbdd1c4faa95d3c5c00b68801d49bb6
pham.huy.cuong <SUN-ASTERISK\pham.huy.cuong@b120290-lt.sun-asterisk.com>
2025-03-30 21:43:02 +0700

    v1-add-headerbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,39 @@
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./pages/home";
 import Analyst from "./pages/analyst";
 import React from "react";
+import { AppBar, Box, IconButton, Toolbar, Typography } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import Button from "@mui/material/Button";
 
 export default function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/analyst/">Analyst</Link>
-            </li>
-          </ul>
-        </nav>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="static">
+          <Toolbar sx={{ display: "flex", justifyContent: "left" }}>
+            <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Button component={Link} to="/" variant="text">
+              <Typography sx={{ color: "white", mr: 2 }}>Home</Typography>
+            </Button>
+            <Button component={Link} to="/analyst/" variant="text">
+              <Typography sx={{ color: "white" }}>Analyst</Typography>
+            </Button>
+          </Toolbar>
+        </AppBar>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/analyst/" element={<Analyst />} />
         </Routes>
-      </div>
+      </Box>
     </Router>
   );
 }
