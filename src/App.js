bba62b5f2793236ac7c3c49dbb2296b3866e995d
pham.huy.cuong <SUN-ASTERISK\pham.huy.cuong@b120290-lt.sun-asterisk.com>
2025-03-30 20:59:28 +0700

    v1-add-routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  Routes,
+} from "react-router-dom";
+import Home from "./pages/home";
+import Analyst from "./pages/analyst";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/analyst/">Analyst</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/" element={<Home />} exact />
+          <Route path="/analyst/" element={<Analyst />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
-
-export default App;
