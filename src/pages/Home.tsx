b61e9682e9f592fd502ff1839b4dc00684a59326
pham.huy.cuong <SUN-ASTERISK\pham.huy.cuong@b120290-lt.sun-asterisk.com>
2025-10-06 13:54:40 +0700

    deploy

--- src/pages/Home.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import WithControlPanel from "../hoc/WithControlPanel";
-
-const Home: React.FC = function () {
-  return <div>Home</div>;
-};
-
-export default WithControlPanel(Home);
